0a8f951351509191c9d735bc17a2f17797522332
hqdem <ilyadem4@gmail.com>
2023-02-27 18:27:28 +0300

    add base models (File and Directory) to the projects. add models to the admin panel

--- api/models.py
+++ api/models.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class File(models.Model):
+    file = models.FileField(upload_to='files/%Y/%m/%d/', verbose_name='Файл')
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='files',
+                              verbose_name='Пользователь')
+
+    class Meta:
+        verbose_name = 'Файл'
+        verbose_name_plural = 'Файлы'
+
+    def __str__(self):
+        return self.file.name
+
+
+class Directory(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название')
+    files = models.ManyToManyField('File', blank=True, related_name='dirs', verbose_name='Файлы')
+    parent_dir = models.ForeignKey('self', on_delete=models.CASCADE, related_name='children',
+                                   verbose_name='Родительская директория')
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='dirs',
+                              verbose_name='Пользователь')
+
+    class Meta:
+        verbose_name = 'Папка'
+        verbose_name_plural = 'Папки'
+
+    def __str__(self):
+        return self.name
