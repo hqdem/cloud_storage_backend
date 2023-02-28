7be33139497e103748c1f018a222b8c4b9c19007
hqdem <ilyadem4@gmail.com>
2023-02-28 16:56:12 +0300

    add possibility to add multiply files to certain dir

--- api/viewsets/directory_viewset.py
+++ api/viewsets/directory_viewset.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets, status
 from rest_framework import permissions
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from ..models import Directory
+from ..models import Directory, File
 from ..serializers.directory_serializers import DirectorySerializer, DirectoryCreateSerializer
 
 
@@ -28,3 +29,17 @@ class DirectoryViewSet(viewsets.ModelViewSet):
                 return obj
             return Response(DirectorySerializer(obj).data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=True, methods=['post'])
+    def add_files(self, request, pk):
+        files = request.FILES.getlist('files')
+        owner = request.user
+        directory = self.get_object()
+
+        files_obj_list = []
+        for file in files:
+            files_obj_list.append(File(file=file, owner=owner))
+        created_objs = File.objects.bulk_create(files_obj_list)
+
+        directory.files.add(*created_objs)
+        return Response(status=status.HTTP_204_NO_CONTENT)
