9cb4f751177b61eb375ad6ed7aacde309212eb13
hqdem <ilyadem4@gmail.com>
2023-02-28 15:27:39 +0300

    add create endpoint for Directory model

--- api/serializers/directory_serializers.py
+++ api/serializers/directory_serializers.py
@@ -1,4 +1,6 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
+from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.response import Response
 
 from ..models import Directory
 from ..serializers.file_serializers import FileSerializer
@@ -15,7 +17,7 @@ class SubdirectorySerializer(serializers.ModelSerializer):
 
 
 class DirectorySerializer(serializers.ModelSerializer):
-    files = FileSerializer(many=True)
+    files = FileSerializer(many=True, read_only=True)
     owner = UserSerializer(read_only=True)
     children_dirs = SubdirectorySerializer(many=True, read_only=True, source='children')
 
@@ -28,3 +30,34 @@ class DirectorySerializer(serializers.ModelSerializer):
             'files',
             'owner'
         ]
+
+
+class DirectoryCreateSerializer(serializers.ModelSerializer):
+    parent_dir_id = serializers.IntegerField(required=False, allow_null=True)
+
+    class Meta:
+        model = Directory
+        fields = [
+            'name',
+            'parent_dir_id'
+        ]
+
+    def create(self, validated_data):
+        user = self.context.get('user')
+
+        dir_name = validated_data.get('name')
+        parent_dir_id = validated_data.get('parent_dir_id', None)
+
+        directory = Directory(name=dir_name, owner=user)
+
+        if parent_dir_id is not None:
+            try:
+                parent_dir = Directory.objects.get(id=parent_dir_id)
+                directory.parent_dir=parent_dir
+            except ObjectDoesNotExist:
+                return Response({'detail': {
+                    'parent_dir_id': "Directory with that id doesn't exist"
+                }}, status=status.HTTP_400_BAD_REQUEST)
+
+        directory.save()
+        return directory
--- api/viewsets/directory_viewset.py
+++ api/viewsets/directory_viewset.py
@@ -1,8 +1,9 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework import permissions
+from rest_framework.response import Response
 
 from ..models import Directory
-from ..serializers.directory_serializers import DirectorySerializer
+from ..serializers.directory_serializers import DirectorySerializer, DirectoryCreateSerializer
 
 
 class DirectoryViewSet(viewsets.ModelViewSet):
@@ -12,4 +13,18 @@ class DirectoryViewSet(viewsets.ModelViewSet):
         return Directory.objects.select_related('owner').prefetch_related('files', 'files__owner', 'children').all()
 
     def get_serializer_class(self):
+        if self.action == 'create':
+            return DirectoryCreateSerializer
         return DirectorySerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+
+        serializer = self.get_serializer(data=data, context={'user': request.user})
+        if serializer.is_valid():
+            obj = serializer.save()
+
+            if isinstance(obj, Response):
+                return obj
+            return Response(DirectorySerializer(obj).data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
