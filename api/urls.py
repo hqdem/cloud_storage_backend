82a7e57eeeb556f4d3791110929cdde28c741bd5
hqdem <ilyadem4@gmail.com>
2023-03-12 10:40:24 +0300

    move auth urls to the api app

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,8 @@
+from django.urls import re_path, include
+
 from .router import router
 
 urlpatterns = [
-
+    re_path(r'^auth/', include('djoser.urls')),
+    re_path(r'^auth/', include('djoser.urls.jwt')),
 ] + router.urls
